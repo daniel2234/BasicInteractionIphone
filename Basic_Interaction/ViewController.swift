2285ff14ba6116ab68b6618d80a5a19bbcb4befe
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2017-06-05 19:13:33 -0400

    created basic iPhone interaction

--- Basic_Interaction/ViewController.swift
+++ Basic_Interaction/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        textLabel.text = textField.text
+    }
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textField.text = ""
+        textLabel.text = ""
+    }
 
 }
 
