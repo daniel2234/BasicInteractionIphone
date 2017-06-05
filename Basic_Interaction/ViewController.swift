ea079c32bd7c5cbb3ce0f9b2fb4dd3009e5dde46
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2017-06-05 18:47:51 -0400

    Initial Commit

---
+++ Basic_Interaction/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Basic_Interaction
+//
+//  Created by Daniel Kwiatkowski on 2017-06-05.
+//  Copyright © 2017 Spiffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
